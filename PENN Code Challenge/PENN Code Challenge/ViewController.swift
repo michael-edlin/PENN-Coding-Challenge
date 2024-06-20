c705eb1ce11af08fda269eb9a3f276484c122928
Tyler Edlin <edlin.michael1@gmail.com>
2024-06-20 16:13:00 -0400

    Initializes project structure

---
+++ PENN Code Challenge/PENN Code Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PENN Code Challenge
+//
+//  Created by Tyler Edlin on 6/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PENN Code Challenge/PENN Code ChallengeTests/PENN_Code_ChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PENN_Code_ChallengeTests.swift
+//  PENN Code ChallengeTests
+//
+//  Created by Tyler Edlin on 6/20/24.
+//
+
+import Testing
+@testable import PENN_Code_Challenge
+
+struct PENN_Code_ChallengeTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
