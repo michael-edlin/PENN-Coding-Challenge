38b9bdceff90ebc693879681ccbbf8ce47e6a8d7
Tyler Edlin <edlin.michael1@gmail.com>
2024-06-20 16:28:19 -0400

    Creates first ViewController Screen

---
+++ PENN Code Challenge/PENN Code Challenge/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PENN Code Challenge
+//
+//  Created by Tyler Edlin on 6/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "AQI Fetcher"
+    }
+}
+
