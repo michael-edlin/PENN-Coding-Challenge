f4d808f3edbb4ac6cecc448966de1e30d2d58d16
Tyler Edlin <edlin.michael1@gmail.com>
2024-06-20 16:27:40 -0400

    Initializes project structure

---
+++ PENN Code Challenge/PENN Code ChallengeUITests/PENN_Code_ChallengeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PENN_Code_ChallengeUITestsLaunchTests.swift
+//  PENN Code ChallengeUITests
+//
+//  Created by Tyler Edlin on 6/20/24.
+//
+
+import XCTest
+
+final class PENN_Code_ChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
